1542e9e084302064217e4b17bb81f7072edd45a2
Rodrigo O. Polo <r.ortegapolo@uleth.ca>
2016-07-13 22:43:47 -0600

    Use defaultdict, create pandas dataframes, compare vs. orginal data

--- metaextract.py
+++ metaextract.py
@@ -1,6 +1,7 @@
 from Bio import Entrez
 from Bio import SeqIO
-
+from collections import defaultdict
+import pandas as pd
 
 # See if there is an alternative to download this
 Entrez.email = "r.ortegapolo@uleth.ca"
@@ -9,12 +10,22 @@ Entrez.email = "r.ortegapolo@uleth.ca"
 
 id_list = 
 
+serotype_dict = defaultdict(list)
+
 handle = Entrez.efetch(db="nuccore", id=id_list, rettype="gb", retmode="text")
 
 for record in SeqIO.parse(handle, "gb"):
-    sero = 'Untyped'
+    serotype = 'Untyped'
     for feature in record.features:
         if 'serotype' in feature.qualifiers:
-            sero = feature.qualifiers['serotype']
-    print(record.name, sero)
+            serotype = feature.qualifiers['serotype']
+    sero_dict[record.name].append(serotype)
+
 handle.close()
+
+sero_DF = pd.DataFrame(sero_dict, index=(['Serotype'])).T
+sero_DF.index.name = 'Accession'
+
+sero_DF.to_csv('sero_DF.csv')
+
+# A way to get serotype information from the title or the serotype field
